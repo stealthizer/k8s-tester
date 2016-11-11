61fc3e5496969db1e0be5cc4586743279ce4939e
Jordi Franco <jordi.franco@scmspain.com>
2016-11-11 21:07:09 +0100

    app.py

--- app.py
+++ app.py
@@ -7,14 +7,18 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 
 def GetContainerName():
-    for line in open("/proc/self/cgroup"):
+    try:
+        for line in open("/proc/self/cgroup"):
+    except:
+        return "error"
+    else:
         if 'docker' in line:
-        return line
+    return line
 
 def hello():
     redis.incr('hits')
     container_name = GetContainerName()
-    return 'Hello World! I have been seen %s times. I am %s' % redis.get('hits'), container_name
+    return 'Hello World! I have been seen %s times. I am %s' % (redis.get('hits'), container_name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
