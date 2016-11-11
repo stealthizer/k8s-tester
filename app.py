37c25507714c8e5132092b082d5b1a1f0f8bc723
Jordi Franco <jordi.franco@scmspain.com>
2016-11-11 21:53:23 +0100

    print container_id

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 from flask import Flask
 from redis import Redis
-import os
+
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
-
-def GetContainerName():
+def hello():
     try:
         for line in open("/proc/self/cgroup"):
             if 'docker' in line:
-                return line
+                container_id = line.split('/')[2]
     except:
-        return "error"
-
-def hello():
+        print "error"
     redis.incr('hits')
-    container_name = GetContainerName()
-    return 'Hello World! I have been seen %s times. I am %s' % (redis.get('hits'), container_name)
+    return 'Hello World! I have been seen %s times. I am %s' % (redis.get('hits'), container_id)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
