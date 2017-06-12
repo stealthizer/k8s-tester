5bad68410ff33a575085d4fc19b2d2f8b1f2ef9f
Jordi Franco <jordi.franco@scmspain.com>
2017-06-12 09:33:08 +0200

    replaced container id with container hostname

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 from flask import Flask
 from redis import Redis
+import os
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
-
+container_name = os.environ['HOSTNAME']
 @app.route('/counter')
 def hello():
-    try:
-        for line in open("/proc/self/cgroup"):
-            if 'docker' in line:
-                container_id = line.split('/')[2]
-    except:
-        print "error"
+
     redis.incr('hits')
-    return 'Hello World! I have been seen %s times. I am %s' % (redis.get('hits'), container_id)
+    return 'Hello World! I have been seen %s times. I am %s' % (redis.get('hits'), container_name)
 
 @app.route('/')
 def gatetes():
-    return '<img src="http://thecatapi.com/api/images/get?format=src&type=gif">'
+    return '<CENTER>GATETES!<BR><img src="http://thecatapi.com/api/images/get?format=src&type=gif"><BR>Server Id: ' + container_name
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
